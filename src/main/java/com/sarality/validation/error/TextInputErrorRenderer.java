854a49757611ff0e826d2d8ffd90e322b7345071
Abhideep Singh <abhi@dothat.in>
2017-01-08 05:28:46 +0530

    Fix Bug in Android which introduces empty space after error is reset in TextInputLayout

--- src/main/java/com/sarality/validation/error/TextInputErrorRenderer.java
+++ src/main/java/com/sarality/validation/error/TextInputErrorRenderer.java
@@ -2,6 +2,7 @@ package com.sarality.validation.error;
 
 import android.app.Activity;
 import android.support.design.widget.TextInputLayout;
+import android.view.View;
 
 /**
  * Displays the error is a {@link TextInputLayout} for a Material Design look and feel.
@@ -30,6 +31,11 @@ public class TextInputErrorRenderer implements ErrorRenderer {
 
   @Override
   public void resetError(Activity activity) {
+    layout.setError(null);
     layout.setErrorEnabled(false);
+    // Error in Android implementation introduces extra child which shows up as extra spacing
+    if (layout.getChildCount() == 2) {
+      layout.getChildAt(1).setVisibility(View.GONE);
+    }
   }
 }
